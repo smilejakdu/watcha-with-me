58cf6482592c421bc6a5c588d52396d57ac3a55d
smilejakdu <ash982416@gmail.com>
2021-05-30 03:19:47 +0900

    scheduler_board_model

---
+++ watcha_back/board/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from users.models import User
+# Create your models here.
+
+class Board(models.Model):
+    title   = models.CharField(max_length=200)
+    content = models.CharField(max_length=250)
+    user    = models.ForeignKey(User , on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'boards'
+
+class BoardImg(models.Model):
+    board    = models.ForeignKey(Board , on_delete=models.CASCADE)
+    img_path = models.CharField(max_length=250)
+
+    class Meta:
+        db_table = 'board_imgs'
--- watcha_back/watcha_back/settings.py
+++ watcha_back/watcha_back/settings.py
@@ -30,6 +30,7 @@ INSTALLED_APPS = [
     'django_extensions',
     'users',
     'scheduler',
+    'board'
 ]
 
 MIDDLEWARE = [
