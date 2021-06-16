7b05c9e0a0d28b8fd5431f83351eafeb7c37cc1f
smilejakdu <ash982416@gmail.com>
2021-06-17 02:47:12 +0900

    chart_js setting

---
+++ watcha-front/src/pages/AnalysisPage/AnalysisPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import BarChart from '../../components/BarChart/BarChart'
+
+const AnalysisPage=()=> {
+	return (
+		<div>
+			<BarChart/>
+		</div>
+	)
+}
+
+export default AnalysisPage
