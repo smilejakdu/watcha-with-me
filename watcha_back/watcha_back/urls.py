5459954efd931eb7de2dcbcf725b0d867608d90d
smilejakdu <ash982416@gmail.com>
2021-06-18 02:10:55 +0900

    scheduler url

--- watcha_back/watcha_back/urls.py
+++ watcha_back/watcha_back/urls.py
@@ -3,10 +3,10 @@ from django.conf.urls.static import static
 from django.conf             import settings
 
 urlpatterns = [
-    path('users/'    , include("users.urls")),
+    path('users/'     , include("users.urls")),
     path('scheduler/' , include("scheduler.urls")),
-    path('board/'    , include("board.urls")),
-    path('review'    , include("review.urls")),
+    path('board/'     , include("board.urls")),
+    path('review'     , include("review.urls")),
 ]
 urlpatterns += static(settings.MEDIA_URL,
                     document_root=settings.MEDIA_ROOT)
